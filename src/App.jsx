d6b69bc41f485a9a4d69a0187bb60b49506e1993
laurence1Mas <119684303+laurence1Mas@users.noreply.github.com>
2026-02-08 13:19:38 +0100

    feat: TrendingProducts section added  by using card,cardContent for shadcn and star icon for lucide-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/layouts/Header";
+import TrendingProducts from "./components/sections/TrendingProduct";
 import "./style.css";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
             <Header />
             <main className="flex-1">
-
+                <TrendingProducts />
             </main>
         </div>
     );
